0b58fe2d0c3d046165a2b4551c776ac81a2f364a
Alexander Montag <almontag@tsn.at>
2019-02-26 10:44:38 +0100

    UserController und Registration View erstellt

---
+++ Webanwendung/Webanwendung/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webanwendung.Models;
+using Webanwendung.Models.db;
+
+namespace Webanwendung.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository userRepository;
+
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            User u = new User(); 
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Registration(User userDataForm)
+        {
+            if(userDataForm == null)
+            {
+                return RedirectToAction("Registration");
+            }
+
+            ValidateRegistration(userDataForm);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    userRepository = new UserRepositoryDB();
+                    userRepository.Open();
+                    userRepository.Insert(userDataForm);
+                    return View("Index");
+                }
+                catch(Exception ex)
+                {
+                    return View("Index");
+                }
+                finally
+                {
+                    userRepository.Close();
+                }
+            }
+            else
+            {
+                return View(userDataForm);
+            }
+        }
+
+        private void ValidateRegistration(User user)
+        {
+            if((user.Firstname != null) && (user.Firstname.Trim().Length < 3))
+            {
+                ModelState.AddModelError("Firstname", "Der Vorname muss mindestens 3 Zeichen lang sein");
+            }
+            if((user.Lastname != null) && (user.Lastname.Trim().Length < 3))
+            {
+                ModelState.AddModelError("Lastname", "Der Nachname muss mindestens 3 Zeichen lang sein");
+            }
+            if(user.Birthdate < DateTime.Now)
+            {
+                ModelState.AddModelError("Birthdate", "Bitte Geburtsdatum angeben");
+            }
+            if(user.Gender == Gender.notSpecified)
+            {
+                ModelState.AddModelError("Gender", "Das Geschlecht muss angegeben werden");
+            }
+            if((user.Username != null) && (user.Username.Trim().Length < 8))
+            {
+                ModelState.AddModelError("Username", "Der Username muss mindestens 8 Zeichen lang sein");
+            }
+            if((user.Email != null) && (!user.Email.Contains('@')))
+            {
+                ModelState.AddModelError("Email", "Bitte Email-Adresse angeben");
+            }
+            if ((user.Password == null) || (user.Password.Length < 8)
+                    || (user.Password.IndexOfAny(new char[] { '!', '?', '%', '&' }) == -1))
+            {
+                ModelState.AddModelError("Password", "Das Passwort muss mindestens 8 Zeichen lang sein und mindestend ein Sonderzeichen einthalten");
+            }
+        }
+    }
+}
--- Webanwendung/Webanwendung/Models/db/UserRepositoryDB.cs
+++ Webanwendung/Webanwendung/Models/db/UserRepositoryDB.cs
@@ -1,30 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace Webanwendung.Models.db
 {
     public class UserRepositoryDB : IUserRepository
     {
         private string _connectionString = "Server=localhost;Database=webProject;Uid=root;Pwd=iscoregoals"; // pw isg
-        private SqlConnection _connection;
+        private MySqlConnection _connection;
 
         public void Open()
         {
-            throw new NotImplementedException();
+            if (this._connection == null)
+            {
+                this._connection = new MySqlConnection(this._connectionString);
+            }
+
+            if (this._connection.State != ConnectionState.Open)
+            {
+                this._connection.Open();
+            }
         }
 
         public void Close()
         {
-            throw new NotImplementedException();
+            if ((this._connection != null) && (this._connection.State == ConnectionState.Open))
+            {
+                this._connection.Close();
+            }
         }
 
         public bool Insert(User userToInsert)
         {
-            throw new NotImplementedException();
+            if (userToInsert == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                MySqlCommand cmd = this._connection.CreateCommand();
+                cmd.CommandText = "INSERT into users values(null, @firstname, @lastname, @birthdate, @gender, @username, @email, sha1(@pwd))";
+                cmd.Parameters.AddWithValue("firstname", userToInsert.Firstname);
+                cmd.Parameters.AddWithValue("lastname", userToInsert.Lastname);
+                cmd.Parameters.AddWithValue("birthdate", userToInsert.Birthdate);
+                cmd.Parameters.AddWithValue("gender", userToInsert.Gender);
+                cmd.Parameters.AddWithValue("username", userToInsert.Username);
+                cmd.Parameters.AddWithValue("email", userToInsert.Email);
+                cmd.Parameters.AddWithValue("pwd", userToInsert.Password);
+
+                return cmd.ExecuteNonQuery() == 1;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
+
+
+
 
 
     }
