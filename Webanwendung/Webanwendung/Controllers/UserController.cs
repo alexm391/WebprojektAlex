c132e39a944baacf2ef5dca0982f39e4c94a71ff
Alexander Montag <almontag@tsn.at>
2019-03-06 14:44:25 +0100

    Logout Methode im UserController erstellt und Layout für Anmeldung angepasst

--- Webanwendung/Webanwendung/Controllers/UserController.cs
+++ Webanwendung/Webanwendung/Controllers/UserController.cs
@@ -84,7 +84,7 @@ namespace Webanwendung.Controllers
                     }
                     else
                     {
-                        Session["isAdmin"] = false;
+                        Session["isRegisteredUser"] = true;
                         Session["name"] = user.Firstname + " " + user.Lastname;
                         //Session["id"] = user.ID;
                     }
@@ -108,15 +108,24 @@ namespace Webanwendung.Controllers
             }
         }
 
+        public ActionResult Logout()
+        {
+            Session["isAdmin"] = null;
+            Session["isRegisteredUser"] = null; 
+            Session["name"] = null;
+            //Session["id"] = null;
+            return View("Message", new Message("Abmeldung", "Sie wurden erfolgreich abgemeldet"));
+
+        }
 
 
         private void ValidateRegistration(User user, bool pwd)
         {
-            if((user.Firstname != null) && (user.Firstname.Trim().Length < 3))
+            if((user.Firstname == null) || (user.Firstname.Trim().Length < 3))
             {
                 ModelState.AddModelError("Firstname", "Der Vorname muss mindestens 3 Zeichen lang sein");
             }
-            if((user.Lastname != null) && (user.Lastname.Trim().Length < 3))
+            if((user.Lastname == null) || (user.Lastname.Trim().Length < 3))
             {
                 ModelState.AddModelError("Lastname", "Der Nachname muss mindestens 3 Zeichen lang sein");
             }
@@ -128,11 +137,11 @@ namespace Webanwendung.Controllers
             {
                 ModelState.AddModelError("Gender", "Das Geschlecht muss angegeben werden");
             }
-            if((user.Username != null) && (user.Username.Trim().Length < 8))
+            if((user.Username == null) || (user.Username.Trim().Length < 8))
             {
                 ModelState.AddModelError("Username", "Der Username muss mindestens 8 Zeichen lang sein");
             }
-            if((user.Email != null) && (!user.Email.Contains('@')))
+            if((user.Email == null) || (!user.Email.Contains('@')))
             {
                 ModelState.AddModelError("Email", "Bitte Email-Adresse angeben");
             }
