588650544c69db20fc20f015f8891755a2a494c0
Alexander Montag <almontag@tsn.at>
2019-03-26 08:47:55 +0100

    Tabelle rooms und bookings erstellt

--- Webanwendung/Webanwendung/Models/db/sql/webProject.sql
+++ Webanwendung/Webanwendung/Models/db/sql/webProject.sql
@@ -14,5 +14,33 @@ create table users(
     constraint id_PK primary key(id)
 )engine=InnoDB;
 
+create table rooms(
+	roomNr int not null unique,
+    beds int not null,
+    price decimal not null,
+    constraint roomNr_PK primary key(roomNr)
+)engine=InnoDB;
+
+create table bookings(
+	id int not null auto_increment,
+    idUser int not null,
+    roomNr int not null,
+    startDate date not null,
+    endDate date not null,
+    constraint idUser_FK foreign key(idUser) references users(id),
+	constraint roomNr_FK foreign key(roomNr) references rooms(roomNr),
+	constraint id_PK primary key(id)
+)engine=InnoDB;
+
 insert into users values(null, "admin", "admin", null, 0, "admin", "admin@admin.com", sha1("admin!"), true);
+
+insert into rooms values(1, 1, 30);
+insert into rooms values(2, 2, 40);
+insert into rooms values(3, 3, 50);
+
+insert into bookings values(null, 2, 1, "2019-04-20", "2019-04-22")
+
+
+
+
 
