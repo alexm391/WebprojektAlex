f3faa9cc6616a76ceef9c93e4ac25d6ca0c43e26
Alexander Montag <almontag@tsn.at>
2019-03-26 09:07:18 +0100

    Booking.cs und IBookingRepository.cs erstellt

---
+++ Webanwendung/Webanwendung/Models/Booking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webanwendung.Models
+{
+    public class Booking
+    {
+        int ID { get; set; }
+        int IdUser { get; set; }
+        int RoomNr { get; set; }
+        DateTime StartDate { get; set; }
+        DateTime EndDate { get; set; }
+
+        public Booking() : this(-1, 0, 0, DateTime.MinValue, DateTime.MinValue) { }
+        public Booking(int id, int idUser, int roomNr, DateTime startDate, DateTime endDate)
+        {
+            this.ID = id;
+            this.IdUser = idUser;
+            this.RoomNr = roomNr;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+
+    }
+}
---
+++ Webanwendung/Webanwendung/Models/db/IBookingRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webanwendung.Models.db
+{
+    interface IBookingRepository
+    {
+        void Open();
+        void Close();
+        bool Insert(Booking bookingToInsert);
+        // bool Delete();
+        bool CheckAvailability(DateTime startDate, DateTime endDate, int beds);
+
+    }
+}
